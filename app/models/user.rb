f652adc7e04288ad56f148dddea2cd5fee47faef
Michael <michael.b.s.rossiter@gmail.com>
2014-03-24 18:21:14 -0700

    Ready to push

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
   validates :profile_name, presence: true,
                            uniqueness: true,
                            format: {
