01418a8ab741125e7d0697cd83de8b3b51c68884
Michael <michael.b.s.rossiter@gmail.com>
2014-03-28 14:58:34 -0700

    profile page view updated, computer going nuts

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Treebook::Application.routes.draw do
   resources :statuses
     get 'feed', to: 'statuses#index', as: :feed
     root 'statuses#index'
-  
+
+
+  get '/:id', to: 'profiles#show'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
